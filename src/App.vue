433123e5055cb19dad33066afd218a4041a2c652
chenzhaohua <863654981@qq.com>
2017-11-16 18:51:33 +0800

    斗鱼项目初试

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <transition name="side">
+      <side-menu v-show="show" @hide="hideSide"></side-menu>
+    </transition>
+    <router-view/>
   </div>
 </template>
 
 <script>
+import SideMenu from './components/SideMenu'
+import bus from './bus'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    SideMenu
+  },
+  created () {
+    this.$http.get(`/douyuapi/RoomApi/live?offset=1&limit=20`)
+    .then(res => {
+      console.log(res.data.data)
+    })
+  },
+  data () {
+    return {
+      show: false
+    }
+  },
+  mounted () {
+    bus.$on('showSide', this.side)
+  },
+  methods: {
+    side () {
+      this.show = !this.show
+    },
+    hideSide () {
+      this.show = false
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .side-enter-active,.side-leave-active,
+  .side-enter-active ul,.side-leave-active ul{
+    transition:all .4s linear;
+  }
+  .side-enter,.side-leave-active{
+    opacity: 0;
+  }
+  .side-enter ul,.side-leave-active ul{
+    transform:translateX(-50%);
+    opacity:0;
+  }
+
+  .load-more {
+    margin: 10px;
+    text-align: center;
+  }
+  .load-more span {
+    display: inline-block;
+    line-height: 30px;
+    padding: 0 20px;
+    border-radius: 10px;
+    border: 1px solid #000;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,19 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Axios from 'axios'
-import VueAxios from 'vue-axios'
 import 'lib-flexible'
 import './assets/css/font-awesome.css'
 import './assets/css/style.css'
+import Axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, Axios)
+
 Vue.config.productionTip = false
-Vue.use(LibFlexible)
-Vue.use(Axios, VueAxios)
-    /* eslint-disable no-new */
+
+Vue.filter('number', v => {
+  if (v > 10000) {
+    return (v / 10000).toFixed(2) + '万'
+  }
+  return v
+})
+Vue.filter('message', v => {
+  if (v.length > 12) {
+    return v.substr(0, 10) + '...'
+  }
+  return v
+})
+
+/* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    render: h => h(App),
-    router,
-    template: '<App/>',
-    components: { App }
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
 })
